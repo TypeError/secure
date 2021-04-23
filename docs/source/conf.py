01f252b8e7358642dc344b187c55a0b3b2ff87bc
cak <cak@fastmail.com>
2021-04-23 06:33:32 -0400

    import secure

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,9 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../"))
+sys.path.insert(0, os.path.abspath(".."))
+
+import secure
 
 
 # -- Project information -----------------------------------------------------
