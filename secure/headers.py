1b3bb84be862ab306cd9e3ca417df10a5c408ea9
Edwin Ismail <typhoon_ed@yahoo.com>
2021-11-20 01:08:16 +0100

    :tada: Clear-Site-Data HTTP header added

--- secure/headers.py
+++ secure/headers.py
@@ -960,3 +960,40 @@ class PermissionsPolicy:
     def xr_spatial_tracking(self, *allowlist: str) -> "PermissionsPolicy":
         self._build("xr-spatial-tracking", *allowlist)
         return self
+
+
+class ClearSiteData:
+    """
+    The Clear-Site-Data header clears browsing data (cookies, storage, cache) associated with the requesting website. It allows web developers to have more control over the data stored by a client browser for their origins.
+    Resources:
+    https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Clear-Site-Data
+    """
+
+    def __init__(self) -> None:
+        self.__policy: List[str] = []
+        self.header = "Clear-Site-Data"
+        self.value = ""
+
+    def _build(self, directive: str) -> None:
+        self.__policy.append(directive)
+        self.value = ", ".join(self.__policy)
+
+    def clear_storage(self) -> "ClearSiteData":
+        self._build("Storage")
+        return self
+
+    def clear_cache(self) -> "ClearSiteData":
+        self._build("Cache")
+        return self
+
+    def clear_cookies(self) -> "ClearSiteData":
+        self._build("Cookies")
+        return self
+
+    def clear_executionContext(self) -> "ClearSiteData":
+        self._build("executionContexts")
+        return self
+
+    def clear_wildcard(self) -> "ClearSiteData":
+        self._build("*")
+        return self
