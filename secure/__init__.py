e516eebb037a84e346b8727ed745f254b2548c0b
cak <cak@fastmail.com>
2024-09-18 06:24:39 -0400

    refactor __init__.py

--- secure/__init__.py
+++ secure/__init__.py
@@ -1,10 +1,27 @@
-from secure.headers import referrer_policy as referrer_policy
-from secure.headers import report_to as report_to
-from secure.headers import server as server
-from secure.headers import strict_transport_security as strict_transport_security
-from secure.headers import x_content_type_options as x_content_type_options
-from secure.headers import x_frame_options as x_frame_options
-from secure.headers.cache_control import CacheControl
-from secure.headers.content_security_policy import ContentSecurityPolicy
-from secure.headers.permissions_policy import PermissionsPolicy
-from secure.secure import Secure as Secure
+from secure.headers import (
+    cache_control,
+    content_security_policy,
+    custom_header,
+    permissions_policy,
+    referrer_policy,
+    report_to,
+    server,
+    strict_transport_security,
+    x_content_type_options,
+    x_frame_options,
+)
+from secure.secure import Secure
+
+__all__ = [
+    "cache_control",
+    "content_security_policy",
+    "custom_header",
+    "permissions_policy",
+    "referrer_policy",
+    "report_to",
+    "server",
+    "strict_transport_security",
+    "x_content_type_options",
+    "x_frame_options",
+    "Secure",
+]
